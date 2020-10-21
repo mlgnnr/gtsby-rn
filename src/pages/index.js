1a099ae4b2daa3f006ae696f7ec3bd90a5fb7fef
mlgnnr <emmigunn@gmail.com>
2020-10-21 09:59:38 +0000

    update txt

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,32 +15,13 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Edda.run</title>
+        <title>edda.run</title>
       </Helmet>
       <h1>30 km fyrir 30 ár</h1>
+      <h2>í boði edda.run</h2>
       <div>
         <iframe src='https://connect.garmin.com/modern/course/embed/44427275' width='465' height='548' frameborder='0'></iframe>
       </div>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
