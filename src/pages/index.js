60923050ebc9577866f52595eabaeb71e4dde974
mlgnnr <emmigunn@gmail.com>
2020-10-23 14:24:22 +0000

    countdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,80 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import DateCountdown from 'react-date-countdown-timer';
 import Lax from "../images/gatsby-icon.png"
 import '../styles/index.css';
+
+const Countdown = () => {
+  const [countdownDate, setCountdownDate] = useState(new Date('2020-10-25T09:00:00').getTime());
+  const [state, setState] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    setInterval(() => setNewTime(), 1000);
+  }, []);
+
+  const setNewTime = () => {
+    if (countdownDate) {
+      const currentTime = new Date().getTime();
+
+      const distanceToDate = countdownDate - currentTime;
+
+      let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
+      let minutes = Math.floor(
+        (distanceToDate % (1000 * 60 * 60)) / (1000 * 60),
+      );
+      let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
+
+      const numbersToAddZeroTo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+      days = `${days}`;
+      if (numbersToAddZeroTo.includes(hours)) {
+        hours = `0${hours}`;
+      } else if (numbersToAddZeroTo.includes(minutes)) {
+        minutes = `0${minutes}`;
+      } else if (numbersToAddZeroTo.includes(seconds)) {
+        seconds = `0${seconds}`;
+      }
+
+      setState({ days: days, hours: hours, minutes, seconds });
+    }
+  };
+
+  return (
+    <div>
+      <div 
+        className='countdown-wrapper'
+        style={{
+            display: 'flex',
+            justifyContent: 'space-evenly'
+        }}>
+        <div className='time-section'>
+          <div className='time'>{state.days || '0'}</div>
+          <small className="time-text">dagar</small>
+        </div>
+        <div className='time-section'>
+          <div className='time'>{state.hours || '00'}</div>
+          <small className="time-text">klst</small>
+        </div>
+        <div className='time-section'>
+          <div className='time'>{state.minutes || '00'}</div>
+          <small className="time-text">mín</small>
+        </div>
+        <div className='time-section'>
+          <div className='time'>{state.seconds || '00'}</div>
+          <small className="time-text">sek</small>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function Index() {
   const [date, setDate] = useState(null);
@@ -19,12 +92,13 @@ function Index() {
       <Helmet>
         <title>edda.run</title>
       </Helmet>
+      <div>
+          <Countdown />
+      </div>
       <img 
         src={Lax}
         style={{width: '100%'}}></img>
       <h1>30 km fyrir 30 ár</h1>
-
-      <p>Vinsamlegast bíðið..</p>
     </main>
   );
 }
