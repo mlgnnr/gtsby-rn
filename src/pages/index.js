f8275755dcf5b2fdc66388ab59cda8204c443150
mlgnnr <emmigunn@gmail.com>
2020-10-23 21:42:46 +0000

    garmin link

--- src/pages/index.js
+++ src/pages/index.js
@@ -129,9 +129,14 @@ function Index() {
                 maxWidth: '100%',
                 borderRadius: '40px',
             }}>
-            <a
-            href="https://connect.garmin.com/modern/course/44427275">
-                Skoða leið nánar
+            <a 
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}
+                href="https://connect.garmin.com/modern/course/44427275">
+                <span>Skoða hlaupakort nánar </span>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>         
             </a>
         </div>
 
