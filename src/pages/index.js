7812c9d88aeb50f3071c138f1e3a5da106ce2916
mlgnnr <emmigunn@gmail.com>
2021-06-04 13:07:25 +0000

    up

--- src/pages/index.js
+++ src/pages/index.js
@@ -153,7 +153,7 @@ function Index() {
         <Laxlink 
           title={"Tímataka"}
           internal={false}
-          url={"https://timataka.net/hengillultra2021/"}>
+          url={"https://timataka.net/hengillultra2021/urslit/?race=1&cat=f"}>
             <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 0 24 24" width="28px" fill="#000000">
               <path d="M0 0h24v24H0V0z" fill="none"/><path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
             </svg>  
